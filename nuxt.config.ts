93769fca56d1e3cc5888742e5530c5b946916e9d
Nitesh Kesarkar <niteshkesarkar136@gmail.com>
2025-02-16 20:48:42 +0530

    Upgrade to nuxt v4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  future: {
+    compatibilityVersion: 4,
+  },
+  devtools: { enabled: true },
+  srcDir: ".",
+  dir: {
+    app: "app",
+  },
+  experimental: {
+    scanPageMeta: "after-resolve",
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    normalizeComponentNames: false,
+    spaLoadingTemplateLocation: "within",
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+  features: {
+    inlineStyles: true,
+  },
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
+});
